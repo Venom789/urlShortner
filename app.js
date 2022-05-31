2f300c8db02403fe7ba504480170bfb21b4b2bb2
Akash Deep <deep98akash@gmail.com>
2022-05-31 11:44:28 +0530

    modified app.js

--- app.js
+++ app.js
@@ -1,71 +1,45 @@
-const uid = () => {
-    return Date.now().toString(16) + Math.random().toString(16).substr(15);
-  };
+const express = require("express");
+const route = require("./routes/index");
+const conncetDB = require("./model/db");
+const notFound = require("./middleware/notFound");
+const { auth } = require("express-openid-connect");
+require("dotenv").config();
+const app = express();
+app.set("view engine", "ejs");
 
-var indexRouter = require("./routes/index.js");
+const PORT = process.env.PORT || 3000;
+const auth_config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
 
-const express = require('express');
-const app= express();
-const mongoose = require('mongoose');
-const { auth } = require('express-openid-connect');
-
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: 'a long, randomly-generated string stored in env',
-    baseURL: 'http://localhost:3000',
-    clientID: 'a2DJzcMC3fb9FAfbQFcDgP56bip5Ngb1',
-    issuerBaseURL: 'https://deep98akash.us.auth0.com'
-  };
-
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-  });
-
-  const { Schema } = mongoose;
-
-const saveUrlSchema = new Schema({
-  url:{type:String,unique:[true,'url already exits'],maxlength:4000},
-  shortenUrl: {type:String,unique:true},
-});
-
-const saveURL = mongoose.model('saveURL', saveUrlSchema);
-
-//require("dotenv").config();
-
+app.use(auth(auth_config));
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
-
-
-//app.use("/",indexRouter);
-
-//app.use(express.static('./public'));
-
-
-
-app.use(auth(config));
-
-const mongoURI = "mongodb+srv://Venom789:222222222@cluster0.unao6.mongodb.net/urlShortner?retryWrites=true&w=majority";
-mongoose.connect(mongoURI).then(()=>{
-    console.log('connected to db');
-}).catch((e)=>{
-    console.log(e);
+app.use("/", route);
+app.get("/isLogin", (req, res) => {
+  if (req.oidc.isAuthenticated()) {
+    res.json({ data: req.oidc.user });
+  } else {
+    res.json({ login: false });
+  }
 });
 
+app.use("*", notFound);
 
-app.set('view engine', 'ejs');
-app.set("views","views");
+const startApp = async () => {
+  try {
+    await conncetDB();
+    app.listen(PORT, () => {
+      console.log(`SERVER IS LISTING AT PORT ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-
-app.listen(3000,()=>{
-    console.log('server started');
-    console.log();
-});
-
-
-
-
-
-
-
+startApp();
